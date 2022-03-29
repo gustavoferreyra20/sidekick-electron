b3fcf289eae6ec3c1614c27ad3e747c9e2dfa698
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2022-03-28 22:19:56 -0300

    Home layout

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,7 @@ var { ipcRenderer } = require('electron');
 var testingdb = document.getElementById('testingdb')
 
 var testContent = document.getElementById('test')
-
+/*
 testingdb.addEventListener('submit', async (e) => {
     try {
         e.preventDefault();
@@ -17,6 +17,17 @@ testingdb.addEventListener('submit', async (e) => {
         console.log(error);
       }
 })
-
+*/
+var els = document.getElementsByClassName('score');
+for (var i = 0; i < els.length; i++) {
+  var cell = els[i];
+  if (cell.textContent <= 20 ) {
+    cell.classList.add('red')
+  } else if(cell.textContent <= 60) {
+    cell.classList.add('orange');
+  } else if(cell.textContent <= 100) {
+    cell.classList.add('green');
+}
+}
 
 

3456e7dca0c8561fd083baaa94e3bb265d489d9a
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2022-05-12 21:46:13 -0300

    password hash

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,10 +1,10 @@
 const { ipcRenderer }= require("electron");
 const { getConnection } = require("../../database");
+const bcrypt = require('bcrypt');
 
 let nombre;
 let email; 
 let password;
-let passwordRepeat;
 let loginForm; 
 let registrationForm;
 let btnToogle;
@@ -15,6 +15,7 @@ window.onload = function() {
   btnToogle = document.getElementById("toggle")
 
   loginForm.addEventListener('submit', () => {
+    event.preventDefault()
     email = document.getElementById("loginForm").elements["email"]
     password = document.getElementById("loginForm").elements["password"]
     const obj = {email:email.value, password:password.value }
@@ -22,13 +23,13 @@ window.onload = function() {
   });
 
   registrationForm.addEventListener('submit', () => {
+    event.preventDefault()
     nombre = document.getElementById("registrationForm").elements["nombre"]
     email = document.getElementById("registrationForm").elements["email"]
     password = document.getElementById("registrationForm").elements["password"]
-    passwordRepeat = document.getElementById("registrationForm").elements["passwordRepeat"]
     const obj = {nombre:nombre.value, email:email.value, password:password.value }
  
-    registerUser(obj)
+    registerUser(obj).then(validatelogin(obj))
   });
 
   btnToogle.onclick = function(e){
@@ -39,15 +40,16 @@ window.onload = function() {
    }
 }
 
-const registerUser = async (obj) => {
-  try {
-    const conn = await getConnection();
-    const sql = "INSERT INTO usuarios (nombre, email, password) values ('" + obj.nombre + "', '" + obj.email + "', '" + obj.password + "')";
-    await conn.query(sql);
-    validatelogin(obj)
-  } catch (error) {
-    console.log(error);
-  }
+async function registerUser(obj){
+    try {
+        if(obj.password.length > 8){
+          saveUser(obj)
+        } else {
+          console.log("Contraseña demasiado corta")
+        }
+    } catch (error) {
+      console.log(error);
+    }
 };
  
 function toggleDisplay(className, displayState){
@@ -57,15 +59,27 @@ function toggleDisplay(className, displayState){
   }
 }
 
-const validatelogin = async (obj) => {
+async function saveUser(obj){
+    const salt = await bcrypt.genSalt();
+    password = await bcrypt.hash(obj.password, salt)
+    const conn = await getConnection();
+    const sql = "INSERT INTO usuarios (nombre, email, password) values ('" + obj.nombre + "', '" + obj.email + "', '" + password + "')";
+    await conn.query(sql);
+}
+
+async function validatelogin(obj){
   try {
     const conn = await getConnection();
-    const sql = "SELECT * FROM usuarios WHERE email=? AND password=?"
-    await conn.query(sql, [obj.email, obj.password], (error, results, fields) => {
+    const sql = "SELECT password FROM usuarios WHERE email=?"
+    await conn.query(sql, [obj.email], (error, result) => {
       if(error){ console.log(error);}
   
-      if(results.length > 0){
-        ipcRenderer.invoke("login", obj)
+      if(result.length > 0){
+        if(bcrypt.compare(obj.password, result[0].password)){
+          ipcRenderer.invoke("login", obj)
+        }else{
+          console.log("Datos incorrectos")
+        }
          
        }else{
          console.log("Datos incorrectos")

6b14590cf4c55fe78feb4c561ab4a92c26c2bbd6
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2023-03-18 12:31:50 -0300

    show posts without loaded them

--- src/app/components/home/homeCtrl.js
+++ src/app/components/home/homeCtrl.js
@@ -1,83 +1,79 @@
-angular.module('myAppHomeCtrl', ['myAppgameCtrl']).controller('homeCtrl', ['$scope', 'posts', 'modes', 'games', 'platforms', 'popups', function($scope, posts, modes, games, platforms, popups){
-    posts.getAll().then(
-        function(response){
-          posts.load(response).then(function(posts){
-                $scope.posts = posts;
-                $scope.$applyAsync();
-            })
-        }
-      )
+angular.module('myAppHomeCtrl', ['myAppgameCtrl']).controller('homeCtrl', ['$scope', 'posts', 'modes', 'games', 'platforms', 'popups', function ($scope, posts, modes, games, platforms, popups) {
+  posts.getAll().then(
+    function (response) {
+      $scope.posts = response;
+      $scope.$applyAsync();
+    }
+  )
 
-      games.getOptions(false).then(function(response){
-        $scope.gameOptions = response;
-        $scope.gameSelected = $scope.gameOptions[0];
-        
-        $scope.setPlatforms();
+  games.getOptions(false).then(function (response) {
+    $scope.gameOptions = response;
+    $scope.gameSelected = $scope.gameOptions[0];
+
+    $scope.setPlatforms();
+    $scope.$applyAsync();
+  });
+
+  modes.getOptions(true).then(function (response) {
+    $scope.modeOptions = response;
+    $scope.modeSelected = $scope.modeOptions[0];
+
+    $scope.$applyAsync();
+  });
+
+  $scope.setPlatforms = function (arg = null) {
+    var game;
+
+    if (arg != null) {
+      game = (arg.value != 'any') ? arg.value : null;
+    } else {
+      game = arg;
+    }
+
+    platforms.getOptions(game, true).then(function (response) {
+      $scope.platformOptions = response;
+      $scope.platformSelected = $scope.platformOptions[0];
+
+      $scope.$applyAsync();
+    });
+
+  };
+
+  $scope.btnSearchPost = function (game, platform, mode) {
+    let params = {};
+
+    if (game.value != 'any') {
+      params.id_game = game.value;
+    }
+
+    if (platform.value != 'any') {
+      params.id_platform = platform.value;
+    }
+
+    if (mode.value != 'any') {
+      params.id_mode = mode.value;
+    }
+
+    posts.getAll(params).then(
+      function (response) {
+        $scope.posts = response;
         $scope.$applyAsync();
-      });
+      }
+    )
 
-      modes.getOptions(true).then(function(response){
-        $scope.modeOptions = response;
-        $scope.modeSelected = $scope.modeOptions[0];
-        
-        $scope.$applyAsync();
-      });
+  };
 
-      $scope.setPlatforms = function(arg = null){
-        var game;
-
-        if(arg != null){
-          game = (arg.value != 'any') ? arg.value : null;
-        }else{
-          game = arg;
+  $scope.btnSubmitApplication = function (id_post) {
+    posts.getApplications({ id_post: id_post, id_user: userSession.id_user, type: 'sended' })
+      .then((res) => {
+        if (res[0]) {
+          popups.alert("Ya existe una solicitud pendiente");
+        } else {
+          posts.addApplication({ id_post: id_post, id_user: userSession.id_user })
+            .then(popups.alert("Solicitud enviada"));
         }
 
-        platforms.getOptions(game, true).then(function(response){
-          $scope.platformOptions = response;
-          $scope.platformSelected = $scope.platformOptions[0];
-          
-          $scope.$applyAsync();
-        }); 
-       
-      }; 
-      
-      $scope.btnSearchPost = function(game, platform, mode){
-        let params = {};
-
-        if(game.value != 'any'){
-          params.id_game = game.value;
-        }
-    
-        if(platform.value != 'any'){
-          params.id_platform = platform.value;
-        }
-    
-        if(mode.value != 'any'){
-          params.id_mode = mode.value;
-        }
-
-        posts.getAll(params).then(
-          function(response){
-              posts.load(response).then(function(response){
-                  $scope.posts = response;
-                  $scope.$applyAsync();
-              })
-          }
-        )
-       
-      };
-      
-      $scope.btnSubmitApplication = function(id_post){
-        posts.getApplications({id_post: id_post, id_user: userSession.id_user, type: 'sended'})
-        .then((res) => {
-          if(res[0]){
-            popups.alert("Ya existe una solicitud pendiente");
-          }else{
-            posts.addApplication({id_post: id_post, id_user: userSession.id_user})
-            .then(popups.alert("Solicitud enviada"));     
-          }
-         
-        })  
-      };  
+      })
+  };
 
 }]);
--- src/app/components/posts/postService.js
+++ src/app/components/posts/postService.js
@@ -1,110 +1,91 @@
 angular.module('myAppPostService', [])
 
-.factory('posts', ['games', 'modes', 'platforms', 'reviews', 'users', 'popups', function(games, modes, platforms, reviews, users, popups){
-	return {
-		getAll: async function(args = null){
-            return new Promise((resolve, reject) =>{
-                const params = new URLSearchParams(args)
-                var url = process.env.SIDEKICK_API + 'posts';
-            
-                if(args !== null){
-                  url = url + '/bo?' + params;
-                }
-            
-                axios.get(url)
-                .then((res) => {
-                  resolve(res.data);
-                })
-                })
-                
-		},
-    load: async function(posts){
-        for (var i=0, n = posts.length; i < n; i++) { // looping over the options
+  .factory('posts', ['popups', function (popups) {
+    return {
+      getAll: async function (args = null) {
+        return new Promise((resolve, reject) => {
+          const params = new URLSearchParams(args)
+          var url = process.env.SIDEKICK_API + 'posts';
 
-            user = await users.get({id_user: posts[i].id_user});
-            reviewStats = await reviews.getAvg({id_user: user[0].id_user});
-            posts[i].abilityScore = (reviewStats[0].abilityScore === undefined ) ? 0 : Math.round(reviewStats[0].abilityScore);
-            posts[i].karmaScore = (reviewStats[0].karmaScore === undefined ) ? 0 : Math.round(reviewStats[0].karmaScore);
-            game = await games.getOne({id_game: posts[i].id_game});
-            platform = await platforms.getAll({id_platform: posts[i].id_platform});
-            mode = await modes.getAll({id_mode: posts[i].id_mode});
-      
-            posts[i].userName = user[0].name;
-            posts[i].mode = utils.capitalizeFirstLetter(mode[0].name);
-            posts[i].platform = platform[0].name;
-            posts[i].gameName = game[0].name;
-            posts[i].gameImg = game[0].img;
-            } 
-          return posts;
-		},
-    save: async function(post){
+          if (args !== null) {
+            url = url + '/bo?' + params;
+          }
+
+          axios.get(url)
+            .then((res) => {
+              resolve(res.data);
+            })
+        })
+
+      },
+      save: async function (post) {
         const url = process.env.SIDEKICK_API + 'posts';
         let data = {
           id_user: userSession.id_user,
           id_game: post.game.value,
-          id_platform:  post.platform.value,
-          id_mode:  post.mode.value,
+          id_platform: post.platform.value,
+          id_mode: post.mode.value,
           requiredUsers: post.userRequire,
           actualUsers: 0,
-          title:  post.title,
+          title: post.title,
           description: (post.description != null) ? post.description : ''
         }
-  
-        axios.post(url, data)
-        .then(() => {
-          popups.function("Anuncio creado con exito", function (){ (location.reload())})
-        })
-        .catch(function(error) {
-          console.log(error);
-        }); 
-		 },
-      remove: async function(id_post){
-          const url = process.env.SIDEKICK_API + 'posts/bo?id_post='+ id_post;
-          await axios.delete(url)
-          .catch(function(error) {
-            console.log(error);
-          });; 
-      },
-      getApplications: async function(args){
-          return new Promise((resolve, reject) =>{
-              var url = process.env.SIDEKICK_API + 'posts/join?';
-              const params = new URLSearchParams(args)
-          
-              axios.get(url + params)
-              .then((res) => {
-                resolve(res.data)
-              })
-              .catch(function(error) {
-                console.log(error);
-              });
-            })
-      },
-      saveApplication: async function(data){
-          const url = process.env.SIDEKICK_API + 'posts/join';
 
-          axios.post(url, data)
+        axios.post(url, data)
+          .then(() => {
+            popups.function("Anuncio creado con exito", function () { (location.reload()) })
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      },
+      remove: async function (id_post) {
+        const url = process.env.SIDEKICK_API + 'posts/bo?id_post=' + id_post;
+        await axios.delete(url)
+          .catch(function (error) {
+            console.log(error);
+          });;
+      },
+      getApplications: async function (args) {
+        return new Promise((resolve, reject) => {
+          var url = process.env.SIDEKICK_API + 'posts/join?';
+          const params = new URLSearchParams(args)
+
+          axios.get(url + params)
+            .then((res) => {
+              resolve(res.data)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+      },
+      saveApplication: async function (data) {
+        const url = process.env.SIDEKICK_API + 'posts/join';
+
+        axios.post(url, data)
           .then(() => {
             popups.alert("Solicitud enviada");
           })
-          .catch(function(error) {
+          .catch(function (error) {
             console.log(error);
-          }); 
+          });
       },
-      removeApplication: async function(id_post, id_user){
-          const url = process.env.SIDEKICK_API + 'posts/join?id_post='+ id_post + '&id_user='+ id_user;
-          await axios.delete(url)
-          .catch(function(error) {
+      removeApplication: async function (id_post, id_user) {
+        const url = process.env.SIDEKICK_API + 'posts/join?id_post=' + id_post + '&id_user=' + id_user;
+        await axios.delete(url)
+          .catch(function (error) {
             console.log(error);
-          });; 
+          });;
       },
-      addApplication: async function(args){
-          const url = process.env.SIDEKICK_API + 'posts/join?';
-          const params = new URLSearchParams(args)
-        
-          await axios.put(url + params)
-          .catch(function(error) {
+      addApplication: async function (args) {
+        const url = process.env.SIDEKICK_API + 'posts/join?';
+        const params = new URLSearchParams(args)
+
+        await axios.put(url + params)
+          .catch(function (error) {
             console.log(error);
-          }); 
+          });
       }
-	};
-}]);
+    };
+  }]);

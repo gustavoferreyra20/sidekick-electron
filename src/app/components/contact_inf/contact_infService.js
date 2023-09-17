80095e1c41106106b1a2745eb99aae9a30991596
gustavoferreyra20 <ferreyragustavo847@gmail.com>
2023-09-16 22:34:21 -0300

    URLSearchParams prevent null params

--- src/app/components/contact_inf/contact_infService.js
+++ src/app/components/contact_inf/contact_infService.js
@@ -1,24 +1,24 @@
 angular.module('myAppContact_infService', [])
 
-.factory('contact_inf', [function(){
-	return {
-		getAll: async function(args = null){
-            return new Promise((resolve, reject) =>{
-                var url = process.env.SIDEKICK_API + 'contact_inf';
-                const params = new URLSearchParams(args)
-            
-                if(args !== null){
-                  url = url + '/bo?' + params;
-                }
-            
-                axios.get(url)
-                .then((res) => {
-                  resolve(res.data)
-                })
-                .catch(function(error) {
-                  console.log(error);
-                });
-              })
-		}
-	};
-}]);
+  .factory('contact_inf', [function () {
+    return {
+      getAll: async function (args = null) {
+        return new Promise((resolve, reject) => {
+          var url = process.env.SIDEKICK_API + 'contact_inf';
+
+          if (args !== null) {
+            const params = new URLSearchParams(args);
+            url = url + '/bo?' + params;
+          }
+
+          axios.get(url)
+            .then((res) => {
+              resolve(res.data)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+      }
+    };
+  }]);
--- src/app/components/modes/modeService.js
+++ src/app/components/modes/modeService.js
@@ -1,41 +1,41 @@
 angular.module('myAppModeService', [])
 
-.factory('modes', [function(){
-	return {
-		getAll: async function(args = null){
-            return new Promise((resolve, reject) =>{
-                var url = process.env.SIDEKICK_API + 'modes';
-                const params = new URLSearchParams(args)
-            
-                if(args !== null){
-                  url = url + '/bo?' + params;
-                }
-            
-                axios.get(url)
-                .then((res) => {
-                  resolve(res.data)
-                })
-                .catch(function(error) {
-                  console.log(error);
-                });
-              })
-		},
-        getOptions: async function(any = false){
+  .factory('modes', [function () {
+    return {
+      getAll: async function (args = null) {
+        return new Promise((resolve, reject) => {
+          var url = process.env.SIDEKICK_API + 'modes';
 
-            var options = []
-            var modes = await this.getAll();
-          
-            if (any){
-              options.push({value: "any", name: "Cualquier mode"});
-            }
-            
-             for (var i=0, n = modes.length; i < n; i++) { // looping over the options
-                if (modes[i]) {
-                  options.push({value: modes[i].id_mode, name: utils.capitalizeFirstLetter(modes[i].name)});
-                }
-            } 
-          
-            return options;
-		}
-	};
-}]);
+          if (args !== null) {
+            const params = new URLSearchParams(args);
+            url = url + '/bo?' + params;
+          }
+
+          axios.get(url)
+            .then((res) => {
+              resolve(res.data)
+            })
+            .catch(function (error) {
+              console.log(error);
+            });
+        })
+      },
+      getOptions: async function (any = false) {
+
+        var options = []
+        var modes = await this.getAll();
+
+        if (any) {
+          options.push({ value: "any", name: "Cualquier mode" });
+        }
+
+        for (var i = 0, n = modes.length; i < n; i++) { // looping over the options
+          if (modes[i]) {
+            options.push({ value: modes[i].id_mode, name: utils.capitalizeFirstLetter(modes[i].name) });
+          }
+        }
+
+        return options;
+      }
+    };
+  }]);

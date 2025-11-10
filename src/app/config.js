62a2f3e23474aa33e24f14954c28e66b3004cc54
Leo Di Primo <roberto.diprimo@davinci.edu.ar>
2025-11-09 22:12:22 -0300

    edit profile photo

--- src/app/config.js
+++ src/app/config.js
@@ -1,4 +1,5 @@
 
 // AngularJS constant for API base URL
 angular.module('myApp')
-  .constant('API_BASE_URL', 'https://sidekick-server-nine.vercel.app/api');
+  .constant('API_BASE_URL', 'https://sidekick-server-nine.vercel.app/api')
+  .constant('DEFAULT_PHOTO_URL', 'https://5tt8qpm5alqclags.public.blob.vercel-storage.com/profile-61-1758415821040.png');
